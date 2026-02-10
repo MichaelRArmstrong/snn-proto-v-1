748743f311db0cd3bab0c44b9f490f80ecddea91
MichaelRArmstrong <160023193+MichaelRArmstrong@users.noreply.github.com>
2026-02-10 17:41:16 +0000

    began refactor
    began refactor by deprecating older versions of scripts and creating new versions for the new program structure, so far only neuron.gd is redone, the rest still to go.

---
+++ Scripts/network.gd
@@ -0,0 +1,2 @@
+extends Node
+# Update order
---
+++ Scripts/neuron.gd
@@ -0,0 +1,32 @@
+extends Node
+# State + Spike logic
+
+class_name Neuron
+
+var v := 0.0 #membrane potential
+var v_rest := 0.0 # resting potential
+var v_thresh := 1.0 #threshold potential
+var leak := 0.95 #decay over time (New Value =max (Current Value-S,T)))
+var spiked := false
+
+var incoming_v := 0.0 #needs to be set before step is called on each neuron
+
+func step(delta: float, input_current = null) -> bool:
+	#integrate
+	if input_current == null:
+		v += incoming_v
+	else:
+		v += input_current #Note: overwrites/ignores the incoming_v
+	
+	#leak
+	v = max(v-leak,v_rest)
+	
+	#fire
+	if v >= v_thresh:
+		v =  0.0
+		spiked = true
+		return true
+	
+	
+	return false
+	
