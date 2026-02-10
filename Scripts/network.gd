d35a203da2ff27ca95e0c6894662aeda09088cc3
MichaelRArmstrong <160023193+MichaelRArmstrong@users.noreply.github.com>
2026-02-10 18:03:28 +0000

    more refactors
    Done most of synapse.gd for now,  started on network.gd

--- Scripts/network.gd
+++ Scripts/network.gd
@@ -1,2 +1,13 @@
 extends Node
 # Update order
+
+#Sensor neurons: 4, 
+#> food on left/right - 2
+#> obstacle proximity - 1
+#> hazard proximity - 1
+
+#Hidden neurons - 16
+#~4 connections per neuron between hidden and hidden, each sensor neuron should connect to a ~2 hidden neurons.
+
+#motor neurons - 2 
+#accumulate spikes and produce continuous motor signals.
--- Scripts/synapse.gd
+++ Scripts/synapse.gd
@@ -1,2 +1,16 @@
 extends Node
 # Weight + Plasticity
+#1. Stores weights
+#2. Knows who it connecs to, (pre- and post-synaptic neurons)
+#3. Adjusts weights using STDP
+
+var weight := 0.0
+var pre_syn_neuron
+var post_syn_neuron
+
+func init(pre, post: Neuron, w = 0.0) -> void:
+		weight = w
+		pre_syn_neuron = pre
+		post_syn_neuron = post
+
+#update func probably for adjusting weights based on incoming values?
