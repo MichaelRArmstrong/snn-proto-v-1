9405ba23c82520f885b328f795a6c22a1bc653bc
MichaelRArmstrong <160023193+MichaelRArmstrong@users.noreply.github.com>
2026-03-12 12:34:41 +0000

    Environment basics setup
    It now populates a circular area in the center of the screen with a set quantity of food, uniformly distributed at random. next up is edge repulsion, for now just hard collision but later if possible repulsion via sensory input.

--- Scripts/environment.gd
+++ Scripts/environment.gd
@@ -1,2 +1,36 @@
 extends Node
 #Enforce environment bounds and spawn food
+
+#Dish bounds
+@export var dish_center := Vector2(270,270)
+@export var dish_radius := 400
+
+#Food
+@export var food_scene: PackedScene
+@export var food_count := 300
+
+func _ready():
+	fill_dish()
+	return
+
+func fill_dish():
+	for i in food_count:
+		spawn_food()
+
+func spawn_food():
+	var pos = random_dish_position()
+	var food = food_scene.instantiate()
+	food.global_position = pos
+	add_child(food)
+	return
+	
+func random_dish_position() -> Vector2:
+	#get a random point in a square that is dish diameter x dish diameter, then check if its within thee circele, (distance from center < radius)
+	var x = randi_range(dish_center.x - dish_radius, dish_center.x + dish_radius)
+	var y = randi_range(dish_center.y - dish_radius, dish_center.y + dish_radius)
+	var rand_pos = Vector2(x,y)
+	while rand_pos.distance_to(dish_center) > dish_radius:
+		x = randi_range(dish_center.x - dish_radius, dish_center.x + dish_radius)
+		y = randi_range(dish_center.y - dish_radius, dish_center.y + dish_radius)
+		rand_pos = Vector2(x,y)
+	return(rand_pos)
