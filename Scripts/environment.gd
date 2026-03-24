a14eef044e4f1258ad2fa0a878c4e5bbc2345c4b
MichaelRArmstrong <160023193+MichaelRArmstrong@users.noreply.github.com>
2026-03-24 17:43:49 +0000

    New field based setup for the dish environment
    populates the dish with a field of points, uses a noise image to decide the values of each point in the grid, later it will be connected to the nutrition value of that particular spot, interpolated from the four nearest points in the grid. Next the sensors need to be set to work in this new environment, then i need to fix the motor neurons so they actually fire

--- Scripts/environment.gd
+++ Scripts/environment.gd
@@ -1,38 +1,34 @@
-extends Node
+extends Node2D
 #Enforce environment bounds and spawn food
 
 #Dish bounds
 @export var dish_center := Vector2(270,270)
 @export var dish_radius := 400
 @export var deadzone_radius := 50
+@export var dish_resolution := 5 #Space between each point in the field
 
-#Food
-@export var food_scene: PackedScene
-@export var deadzone: Node2D
-@export var food_count := 300
+#circular dish shaped field of points, nutrition value lerped between them in a gradient.
+var dish_grid_points := []
+var noise
 
 func _ready():
-	fill_dish()
-	return
-
-func fill_dish():
-	for i in food_count:
-		spawn_food()
-
-func spawn_food():
-	var pos = random_dish_position()
-	var food = food_scene.instantiate()
-	food.global_position = pos
-	add_child(food)
-	return
+	var grid_size = dish_radius * 2
+	var point_count = (grid_size / dish_resolution) * (grid_size / dish_resolution)
+	noise = FastNoiseLite.new()
+	noise.get_image(grid_size,grid_size,false,false,true)
 	
-func random_dish_position() -> Vector2:
-	#get a random point in a square that is dish diameter x dish diameter, then check if its within thee circele, (distance from center < radius)
-	var x = randi_range(dish_center.x - dish_radius, dish_center.x + dish_radius)
-	var y = randi_range(dish_center.y - dish_radius, dish_center.y + dish_radius)
-	var rand_pos = Vector2(x,y)
-	while rand_pos.distance_to(dish_center) > dish_radius or rand_pos.distance_to(deadzone.global_position) < deadzone_radius:
-		x = randi_range(dish_center.x - dish_radius, dish_center.x + dish_radius)
-		y = randi_range(dish_center.y - dish_radius, dish_center.y + dish_radius)
-		rand_pos = Vector2(x,y)
-	return(rand_pos)
+	for	row in range(point_count+1):
+		for col in range(point_count+1):
+			var x = col * dish_resolution
+			var y = row * dish_resolution
+			
+			var offset = Vector2((540 - dish_radius)/2,(540-dish_radius)/2)
+			#check if its in the dish
+			if Vector2(x,y).distance_to(dish_center) <= dish_radius:
+				dish_grid_points.append(Vector2(x,y))
+
+func _draw() -> void:
+	for point in dish_grid_points:
+		draw_circle(point,dish_resolution * 0.5,Color.from_rgba8((255 * -noise.get_noise_2d(point.x, point.y)),(255 * noise.get_noise_2d(point.x, point.y)),(255 * -noise.get_noise_2d(point.x, point.y))))
+		
+	
